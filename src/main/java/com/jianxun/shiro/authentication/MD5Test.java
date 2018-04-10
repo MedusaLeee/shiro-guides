04714545a0d135512292a7d0972e3d78d36f7fa6
jianxun <medusa.leee@gmail.com>
2018-04-10 21:50:38 +0800

    shiro内置Hash函数使用

--- src/main/java/com/jianxun/shiro/authentication/MD5Test.java
+++ src/main/java/com/jianxun/shiro/authentication/MD5Test.java
@@ -1,21 +1,21 @@
 package com.jianxun.shiro.authentication;
 
-import org.apache.shiro.crypto.hash.Md2Hash;
+import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.crypto.hash.SimpleHash;
 
 public class MD5Test {
     public static void main(String[] args) {
         String source = "111111";
         String salt = "qwerty";
-        int hashIteration = 1;
+        int hashIterations = 1;
         // 构造方法中
         // 第一个参数：明文
         // 第二个参数：盐
         // 第三个参数：散列次数
-        Md2Hash md5Hash = new Md2Hash(source, salt, hashIteration);
-        String password_md5 = md5Hash.toHex();
+        Md5Hash md5Hash = new Md5Hash(source, salt, hashIterations);
+        String password_md5 = md5Hash.toString();
         System.out.println("password_md5: " + password_md5);
-        SimpleHash simpleHash = new SimpleHash("sha1", source, salt, hashIteration);
+        SimpleHash simpleHash = new SimpleHash("sha1", source, salt, hashIterations);
         String password_sha1 = simpleHash.toHex();
         System.out.println("password_sha1: " + password_sha1);
     }
